5efa4337a2804fa7be42edfbaa7f45f5a54f1d05
Adnan <adnan.ahmed@a2sv.org>
2026-02-19 17:10:02 +0300

    frequency-tracker

---
+++ frequency-tracker.py
@@ -0,0 +1,73 @@
+class FrequencyTracker:
+
+    def __init__(self):
+        # self.frequency_tracker = []
+        self.numberToFrequency = defaultdict(int)
+        self.frequencyToCount = defaultdict(int)
+
+    def add(self, number: int) -> None:
+        # self.frequency_tracker.append(number)
+
+        # if number in self.numberToFrequency:
+        #     self.frequencyToCount[self.numberToFrequency[number]] -= 1
+        #     if self.frequencyToCount[self.numberToFrequency[number]] == 0:
+        #         del self.frequencyToCount[self.numberToFrequency[number]]
+        # self.numberToFrequency[number] += 1
+        # self.frequencyToCount[self.numberToFrequency[number]] += 1
+
+        numberDict = self.numberToFrequency
+        freqDict = self.frequencyToCount
+
+        if number in numberDict:
+            freqDict[numberDict[number]] -= 1
+            if freqDict[numberDict[number]] == 0:
+                del freqDict[numberDict[number]]
+        numberDict[number] += 1
+        freqDict[numberDict[number]] += 1
+
+
+    def deleteOne(self, number: int) -> None:
+        # if number in self.frequency_tracker:
+        #     i = len(self.frequency_tracker) - 1
+        #     while i >= 0 and self.frequency_tracker[i] != number:
+        #         i -= 1
+        #     self.frequency_tracker.pop(i)
+
+        numberDict = self.numberToFrequency
+        freqDict = self.frequencyToCount
+
+        if number in numberDict:
+            freqDict[numberDict[number]] -= 1
+            if freqDict[numberDict[number]] <= 0:
+                    del freqDict[numberDict[number]]
+
+            numberDict[number] -= 1
+            if numberDict[number] == -1:
+                del numberDict[number]
+
+            freqDict[numberDict[number]] += 1
+
+
+
+    def hasFrequency(self, frequency: int) -> bool:
+        # for number in self.frequency_tracker:
+        #     if self.frequency_tracker.count(number) == frequency:
+        #         return True
+        
+        # return False
+
+        # for freq in self.numberToFrequency.values():
+        #     if freq == frequency:
+        #         return True
+
+        # return False
+
+        # return frequency in self.numberToFrequency.values()
+
+        return frequency in self.frequencyToCount
+
+# Your FrequencyTracker object will be instantiated and called as such:
+# obj = FrequencyTracker()
+# obj.add(number)
+# obj.deleteOne(number)
+# param_3 = obj.hasFrequency(frequency)
