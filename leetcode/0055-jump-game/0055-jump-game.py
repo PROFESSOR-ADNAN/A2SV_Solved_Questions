ec8ac6a8d47519ab3a030e499d3ca3da8a8766a8
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-15 13:33:44 +0300

    Add solution for Jump Game

--- leetcode/0055-jump-game/0055-jump-game.py
+++ leetcode/0055-jump-game/0055-jump-game.py
@@ -16,11 +16,38 @@ class Solution:
         # return False
 
 
+        # furthest = 0
+        # for i in range(len(nums)):
+        #     if i > furthest:
+        #         return False
+
+        #     furthest = max(furthest, i + nums[i])
+
+        # return True
+
+
         furthest = 0
         for i in range(len(nums)):
             if i > furthest:
                 return False
-
             furthest = max(furthest, i + nums[i])
-
+        
         return True
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
