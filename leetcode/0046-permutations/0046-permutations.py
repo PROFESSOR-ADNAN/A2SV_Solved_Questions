10de1cfb2f92c2a97947916257c56ab3bb1a5eb6
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-27 18:03:35 +0300

    Add solution for Permutations

--- leetcode/0046-permutations/0046-permutations.py
+++ leetcode/0046-permutations/0046-permutations.py
@@ -17,10 +17,24 @@ class Solution:
         # backtrack([])
         # return ans
 
+        # ans = []
+        # path = []
 
+        # def backtrack():
+        #     if len(path) == len(nums):
+        #         ans.append(path[:])
+        #         return
 
+        #     for i in range(len(nums)):
+        #         if nums[i] in path:
+        #             continue
 
+        #         path.append(nums[i])
+        #         backtrack()
+        #         path.pop()
 
+        # backtrack()
+        # return ans
 
         ans = []
         path = []
@@ -33,10 +47,12 @@ class Solution:
             for i in range(len(nums)):
                 if nums[i] in path:
                     continue
-
+                
                 path.append(nums[i])
                 backtrack()
                 path.pop()
 
         backtrack()
         return ans
+        
+        
