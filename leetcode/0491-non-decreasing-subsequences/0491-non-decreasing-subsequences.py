dd43240e2a4065511a755c284347773e2c31e11e
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-03-30 16:56:46 +0300

    Add solution for Non-decreasing Subsequences

--- leetcode/0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ leetcode/0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -1,26 +1,54 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        # ans = []
+        # path = []
+
+        # def backtrack(i):
+        #     if len(path) >= 2 and path not in ans:
+        #         for j in range(1, len(path)):
+        #             if path[j] < path[j-1]:
+        #                 return
+        #         ans.append(path[:])
+            
+        #     if i >= len(nums):
+        #         return
+
+        #     path.append(nums[i])
+        #     backtrack(i+1)
+        #     path.pop()
+
+        #     backtrack(i+1)
+
+        # backtrack(0)
+        # return ans
+
+    
         ans = []
         path = []
 
         def backtrack(i):
+            for k in range(1, len(path)):
+                if path[k] < path[k-1]:
+                    return
             if len(path) >= 2 and path not in ans:
-                for j in range(1, len(path)):
-                    if path[j] < path[j-1]:
-                        return
                 ans.append(path[:])
+
             
-            if i >= len(nums):
-                return
+            for j in range(i, len(nums)):
+                path.append(nums[j])
+                backtrack(j+1)
+                path.pop()
 
-
-
-            path.append(nums[i])
-            backtrack(i+1)
-            path.pop()
-
-            backtrack(i+1)
 
         backtrack(0)
         return ans
+
+
+
+
+
+
+
+
+
 
