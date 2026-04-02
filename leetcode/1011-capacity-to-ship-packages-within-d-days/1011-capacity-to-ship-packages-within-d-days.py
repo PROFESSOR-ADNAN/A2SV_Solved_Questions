e0881b39cdd1d9cdd66f83c70c6229d0a9546a14
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-04-02 12:30:19 +0300

    Add solution for Capacity To Ship Packages Within D Days

--- leetcode/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ leetcode/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -16,32 +16,56 @@ class Solution:
 
         # return left
 
-        total = sum(weights)
+        # total = sum(weights)
+
+        # def isValid(val):
+        #     d = 0
+        #     curr = 0
+        #     for weight in weights:
+        #         curr += weight
+        #         if curr == val:
+        #             d += 1
+        #             curr = 0
+        #         elif curr > val:
+        #             curr = weight
+        #             d += 1
+
+        #     if curr > 0:
+        #         d += 1
+            
+        #     return d <= days
+
+        # low, high = max(weights), total
+
+        # while low <= high:
+        #     mid = low + (high - low) // 2
+        #     if isValid(mid):
+        #         high = mid - 1
+        #     else:
+        #         low = mid + 1
+            
+        # return low
+
 
         def isValid(val):
-            d = 0
-            curr = 0
+            load = 0
+            d = 1
             for weight in weights:
-                curr += weight
-                if curr == val:
+                if load + weight > val:
                     d += 1
-                    curr = 0
-                elif curr > val:
-                    curr = weight
-                    d += 1
+                    load = weight
+                else:
+                    load += weight
 
-            if curr > 0:
-                d += 1
-            
             return d <= days
 
-        low, high = max(weights), total
 
+        low, high = max(weights), sum(weights)
         while low <= high:
             mid = low + (high - low) // 2
             if isValid(mid):
                 high = mid - 1
             else:
                 low = mid + 1
-            
+
         return low
