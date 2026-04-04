80e36384bcb22756561655b3406c9255e84468ef
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-04-04 07:44:13 +0300

    Add solution for H-Index

--- leetcode/0274-h-index/0274-h-index.py
+++ leetcode/0274-h-index/0274-h-index.py
@@ -8,17 +8,28 @@ class Solution:
 
         # return 0
 
-        citations.sort(reverse = True)
+        # citations.sort(reverse = True)
 
-        def isValid(ind):
-            return citations[ind-1] >= ind
+        # def isValid(ind):
+        #     return citations[ind-1] >= ind
 
-        low, high = 1, len(citations)
-        while low <= high:
-            mid = low + (high - low) // 2
-            if isValid(mid):
-                low = mid + 1
-            else:
-                high = mid - 1
+        # low, high = 1, len(citations)
+        # while low <= high:
+        #     mid = low + (high - low) // 2
+        #     if isValid(mid):
+        #         low = mid + 1
+        #     else:
+        #         high = mid - 1
         
-        return high
+        # return high
+
+
+        citations.sort()
+        n = len(citations)
+
+        for h in range(n, 0, -1):
+            idx = bisect.bisect_left(citations, h)
+            if n - idx >= h:
+                return h
+
+        return 0
