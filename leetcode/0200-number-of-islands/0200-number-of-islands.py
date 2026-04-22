a0e4b3aed30862eba166a19e4402391e95f6a5ff
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-04-22 21:36:54 +0300

    Add solution for Number of Islands

---
+++ leetcode/0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        Row = len(grid)
+        Col = len(grid[0])
+        direction = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        def inboundAndValid(r, c):
+            return 0 <= r < Row and 0 <= c < Col and grid[r][c] == "1"
+
+        visited = set()
+        def dfs(r, c):
+            visited.add((r, c))
+            for dx, dy in direction:
+                newR, newC = r + dx, c + dy
+                if inboundAndValid(newR, newC) and (newR, newC) not in visited:
+                    dfs(newR, newC)
+
+        island = 0
+        for r in range(Row):
+            for c in range(Col):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    dfs(r, c)
+                    island += 1
+        
+        return island
