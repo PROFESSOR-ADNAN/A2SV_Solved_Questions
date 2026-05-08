c30a85af60ca929cb3d55bac95cc7ba3d7cb9ff1
PROFESSOR-ADNAN <iam.professor.adnan@gmail.com>
2026-05-08 17:25:22 +0300

    Add solution for Last Stone Weight

---
+++ leetcode/1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,24 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # stones.sort()
+        # while len(stones) > 1:
+        #     a = stones.pop()
+        #     b = stones.pop()
+        #     if a != b:
+        #         stones.append(a-b)
+        #     stones.sort()
+
+        # return stones[0]
+
+        maxHeap = [-val for val in stones]
+        heapq.heapify(maxHeap)
+
+        while len(maxHeap) > 1:
+            a = -1 * heapq.heappop(maxHeap)
+            b = -1 * heapq.heappop(maxHeap)
+
+            if a != b:
+                maxHeap.append(-1 * (a-b))
+                heapq.heapify(maxHeap)
+
+        return -1 * heapq.heappop(maxHeap) if maxHeap else 0
